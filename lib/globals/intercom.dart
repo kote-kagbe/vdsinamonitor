843fc75e5d2f7f489c15dcbc650cdf58411c08dd
kote_kagbe <kote.kagbe@gmail.com>
2023-08-22 16:50:16 +0300

    бета логгера

--- lib/globals/intercom.dart
+++ lib/globals/intercom.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:vdsinamonitor/bl/config.dart';
+import 'package:vdsinamonitor/bl/logger.dart';
 
 final appAuthChannel = StreamController<bool>.broadcast();
+final userErrorsChannel = StreamController<bool>.broadcast();
 
 final config = Config();
+final logger = Logger('VDSinaMonitor');
